bb6eee49fed277a5fabea671777bc5a70193fa01
Samuel-Tiruneh <samuelplusweb@gmail.com>
2024-04-04 21:02:33 +0300

    fouth commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { AboutUs, Chef, FindUs, Gallery, Header, Intro, Laurels, SpecialMenu,Footer} from './container';
 import { Navbar } from './components';
 import './App.css';
-
+ 
 const App = () => (
   <div>
     <Navbar />
